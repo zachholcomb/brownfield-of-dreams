431c04402dfa0e4196236fbb037e8db6b8c50965
DeMarcus Kirby <tkd024@gmail.com>
2020-05-05 18:18:57 -0600

    Add Oauth routes

--- config/routes.rb
+++ config/routes.rb
@@ -24,6 +24,9 @@ Rails.application.routes.draw do
     end
   end
 
+  get '/auth/github'
+  get '/auth/github/callback', to: "signup#create"
+
   get '/login', to: "sessions#new"
   post '/login', to: "sessions#create"
   delete '/logout', to: "sessions#destroy"
