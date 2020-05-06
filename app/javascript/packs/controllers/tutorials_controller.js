559e27f62052976e68da6d492f40a36c40e58227
Zach Holcomb <zachholcombmusic@gmail.com>
2020-05-06 12:20:23 -0600

    Add html for import playlist form and javascript for form show action

--- app/javascript/packs/controllers/tutorials_controller.js
+++ app/javascript/packs/controllers/tutorials_controller.js
@@ -47,4 +47,24 @@ export default class extends Controller {
       });
   }
 
+  showPlaylistForm(event) {
+    event.preventDefault();
+
+    var elem = document.getElementById("import-playlist-form");
+    var getHeight = function () {
+      elem.style.display = 'block';
+      var height = elem.scrollHeight + 'px';
+      elem.style.display = '';
+      return height;
+    };
+
+    var height = getHeight();
+    elem.classList.add('is-visible');
+    elem.style.height = height;
+
+    window.setTimeout(function () {
+      elem.style.height = '';
+    }, 350);
+  }
+
 }
