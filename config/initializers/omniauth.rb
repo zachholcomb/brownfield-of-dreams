49d0eae9297f2f9708242dac78f0b3ffbc23299c
DeMarcus Kirby <tkd024@gmail.com>
2020-05-05 18:12:56 -0600

    Add Onmiauth Builder

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,7 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET']
+
+  use OmniAuth::Builder do
+    provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET']
+  end
 end
