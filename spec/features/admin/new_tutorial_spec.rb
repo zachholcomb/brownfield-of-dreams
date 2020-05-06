f3ca2558e2880884bb530a488dd0f21f3269ec7f
Zach Holcomb <zachholcombmusic@gmail.com>
2020-05-06 14:24:00 -0600

    Add test for new tutorial spec happy path

---
+++ spec/features/admin/new_tutorial_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+describe "An admin visting create new tutorials page" do
+  let(:admin) { create(:admin) }
+
+  scenario "can create a new tutorial with correct information" do
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
+
+    visit "/admin/tutorials/new"
+
+    fill_in 'tutorial[title]', with: "How to Play Piano"
+    fill_in 'tutorial[description]', with: "In these videos, I teach you how to play basic scales."
+    fill_in 'tutorial[thumbnail]', with: "https://i.ytimg.com/vi/yfDJ-OAIADo/hqdefault.jpg?sqp=-oaymwEZCNACELwBSFXyq4qpAwsIARUAAIhCGAFwAQ==&amp;rs=AOn4CLALlh19sNew-eYkQpHTvtnCdhdgXg"
+    click_on 'Save'
+
+    expect(Tutorial.count).to eq(1)
+    new_tutorial = Tutorial.last
+
+    expect(current_path).to eq("/tutorials/#{new_tutorial.id}")
+    expect(page).to have_content("Successfully created tutorial.")
+  end
+end
+
