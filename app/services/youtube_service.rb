3a84b8064c5348c44ff4adbd88daf300e9ca11f0
Zach Holcomb <zachholcombmusic@gmail.com>
2020-05-07 13:41:06 -0600

    Add youtube playlist method to youtube_service

--- app/services/youtube_service.rb
+++ app/services/youtube_service.rb
@@ -5,11 +5,37 @@ class YoutubeService
     get_json('youtube/v3/videos', params)
   end
 
+  def playlist_info(id)
+    params = { part: 'contentDetails', playlistId: id, pageToken: '' }
+
+    get_playlist_video_ids('youtube/v3/playlistItems', params)
+  end
+
   private
 
   def get_json(url, params)
     response = conn.get(url, params)
     JSON.parse(response.body, symbolize_names: true)
+  end
+
+  def get_playlist_video_ids(url, params)
+    video_ids = []
+    response = conn.get(url, params)
+    parsed_response = JSON.parse(response.body, symbolize_names: true)
+    parsed_response[:items].each do |item|
+      video_ids << item[:contentDetails][:videoId]
+    end
+    until parsed_response[:nextPageToken].nil?
+      params[:pageToken] = parsed_response[:nextPageToken]
+      response = conn.get(url, params)
+      parsed_response = JSON.parse(response.body, symbolize_names: true)
+      parsed_response[:items].each do |item|
+        video_ids << item[:contentDetails][:videoId]
+      end
+      params[:pageToken] = parsed_response[:nextPageToken]
+    end
+    require 'pry'; binding.pry
+    video_ids
   end
 
   def conn
