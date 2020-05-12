f9e9799e00e071bb279f9e3c659141d7f534ff9b
Zach Holcomb <zachholcombmusic@gmail.com>
2020-05-12 15:24:20 -0600

    Update user video controller to account for vistors

--- app/controllers/user_videos_controller.rb
+++ app/controllers/user_videos_controller.rb
@@ -2,13 +2,14 @@ class UserVideosController < ApplicationController
   def new; end
 
   def create
+    return no_user_failure if current_user.nil?
+
     user_video = UserVideo.new(user_video_params)
     if current_user.user_videos.find_by(video_id: user_video.video_id)
       flash[:error] = 'Already in your bookmarks'
     elsif user_video.save
       flash[:success] = 'Bookmark added to your dashboard!'
     end
-
     redirect_back(fallback_location: root_path)
   end
 
@@ -16,5 +17,10 @@ class UserVideosController < ApplicationController
 
   def user_video_params
     params.permit(:user_id, :video_id)
+  end
+
+  def no_user_failure
+    flash[:error] = 'User must login to bookmark videos.'
+    redirect_back(fallback_location: root_path)
   end
 end
