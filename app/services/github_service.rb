923abd715077cfc0ae7225b28696be919e67e586
Zach Holcomb <zachholcombmusic@gmail.com>
2020-05-11 18:42:17 -0600

    Update github service for use with getting user

--- app/services/github_service.rb
+++ app/services/github_service.rb
@@ -5,21 +5,25 @@ class GithubService
   end
 
   def repos
-    get_json('repos')[0..4]
+    get_json("user/repos?access_token=#{@token}")[0..4]
   end
 
   def followers
-    get_json('followers')
+    get_json("user/followers?access_token=#{@token}")
   end
 
   def followings
-    get_json('following')
+    get_json("user/following?access_token=#{@token}")
+  end 
+
+  def get_user(username)
+    get_json("/users/#{username}")
   end
 
   private
 
   def conn
-    Faraday.new(url: "https://api.github.com/users/#{@username}/") do |req|
+    Faraday.new(url: "https://api.github.com") do |req|
       req.params['token']
       req.adapter Faraday.default_adapter
     end
