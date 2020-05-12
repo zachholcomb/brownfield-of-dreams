6faf90fb74f1bb2d26abfa1278b34f632c730c51
Zach Holcomb <zachholcombmusic@gmail.com>
2020-05-11 20:17:32 -0600

    Account for sad path

--- app/controllers/invites_controller.rb
+++ app/controllers/invites_controller.rb
@@ -7,9 +7,13 @@ class InvitesController < ApplicationController
     gh_service = GithubService.new(current_user.token, current_user.github_user)
     invite = Invite.create(invite_params)
     user_data = gh_service.get_user(params[:github_handle])
-    info = {inviter: current_user.github_user, invitee: user_data[:login]}
-    InviterMailer.invite(info, user_data[:email]).deliver_now
-    flash[:success] = 'Successfully sent invite!'
+    if user_data[:email]
+      info = {inviter: current_user.github_user, invitee: user_data[:login]}
+      InviterMailer.invite(info, user_data[:email]).deliver_now
+      flash[:success] = 'Successfully sent invite!'
+    else
+      flash[:error] = "The Github user you selected doesn't have an email address associated with their account."
+    end
     redirect_to dashboard_path
   end
 
