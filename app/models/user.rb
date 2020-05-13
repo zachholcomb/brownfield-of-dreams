0045b52ca0719ade2d9e2f0717c313f6b966c1cc
DeMarcus Kirby <tkd024@gmail.com>
2020-05-13 10:00:27 -0600

    Update user model to has self referencing relationships

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,14 @@ class User < ApplicationRecord
   has_many :user_videos, dependent: :destroy
   has_many :videos, through: :user_videos
 
+  has_many :friendships, dependent: :destroy
+  has_many :friends, through: :friendships
+  has_many :inverse_friendships,
+           class_name: 'Friendship',
+           foreign_key: 'friend_id',
+           dependent: :destroy
+  has_many :inverse_friends, through: :inverse_friendships, source: :user
+
   validates :email, uniqueness: true, presence: true
   validates :password, presence: true, on: :create
   validates :first_name, presence: true
