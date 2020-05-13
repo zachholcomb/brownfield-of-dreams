5ebf97d85098ac7f40881698b7f3c0565453bc32
Zach Holcomb <zachholcombmusic@gmail.com>
2020-05-13 16:24:31 -0600

    Add test for inviter action mailer

--- spec/mailers/inviter_spec.rb
+++ spec/mailers/inviter_spec.rb
@@ -1,3 +1,21 @@
 require "rails_helper"
 
-RSpec.describe InviterMailer, type: :mailer do; end
+RSpec.describe InviterMailer, type: :mailer do
+  describe "invite" do
+    it "renders the headers" do
+      info = { inviter: 'Zach Holcomb', invitee: 'Demarcus Kirby' }
+      email_address = 'kirbyDD@example.com'
+      email = InviterMailer.invite(info, email_address)
+      expect(email.from).to eq(['no-reply@brownfield-team.com'])
+      expect(email.to).to eq(['kirbyDD@example.com'])
+      expect(email.subject).to eq("#{info[:invitee]} wants you to join Turing Tutorials!")
+    end
+
+    it "renders the body" do
+      info = { inviter: 'Zach Holcomb', invitee: 'Demarcus Kirby' }
+      email_address = 'kirbyDD@example.com'
+      email = InviterMailer.invite(info, email_address)
+      expect(email.body.encoded).to match("Hello Demarcus Kirby")
+    end
+  end
+end
