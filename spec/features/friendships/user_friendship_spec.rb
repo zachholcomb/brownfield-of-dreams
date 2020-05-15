cacdaa4bbee8d401e6e48d2210df6dac76d9aacf
Zach Holcomb <zachholcombmusic@gmail.com>
2020-05-14 23:19:22 -0600

    Update user friendship test to use VCR

--- spec/features/friendships/user_friendship_spec.rb
+++ spec/features/friendships/user_friendship_spec.rb
@@ -1,17 +1,17 @@
 require 'rails_helper'
 
 describe 'As a registered user' do
-  scenario 'I can add friends to my account' do
-    user_params = { email: 'zachholcombmusic@gmail.com',
-                    first_name: 'Zach',
-                    last_name: 'Holcomb',
-                    password: '1234',
-                    role: 0,
-                    github_user: 'zachholcomb',
-                    token: ENV['GITHUB_TOKEN1']
+  before(:each) do
+    @user_params = { email: 'zachholcombmusic@gmail.com',
+                first_name: 'Zach',
+                last_name: 'Holcomb',
+                password: '1234',
+                role: 0,
+                github_user: 'zachholcomb',
+                token: ENV['GITHUB_TOKEN1']
     }
 
-   user_params2 = { email: 'byrdlike@gmail.com',
+   @user_params2 = { email: 'byrdlike@gmail.com',
                     first_name: 'Zach',
                     last_name: 'Holcomb',
                     password: '1234',
@@ -20,22 +20,25 @@ describe 'As a registered user' do
                     token: ENV['GITHUB_TOKEN2']
     }
 
-    user = User.create!(user_params)
-    user2 = User.create!(user_params2)
+    @user = User.create!(@user_params)
+    @user2 = User.create!(@user_params2)
+  end
+  scenario 'I can add friends to my account' do
+    VCR.use_cassette('/user/add_friends') do
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
+      
+      visit '/dashboard'
 
-    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
-    
-    visit '/dashboard'
-
-    within ".followers" do
-      expect(page).to have_content('apple342')
-      click_on 'Add Friend'
-    end
-    
-    expect(current_path).to eq('/dashboard')
-    expect(page).to have_content('Added friend')
-    within ".friends" do
-      expect(page).to have_content('apple342')
+      within ".followers" do
+        expect(page).to have_content('apple342')
+        click_on 'Add Friend'
+      end
+      
+      expect(current_path).to eq('/dashboard')
+      expect(page).to have_content('Added friend')
+      within ".friends" do
+        expect(page).to have_content('apple342')
+      end
     end
   end
 end
